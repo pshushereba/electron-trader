a1e468d8460e60e0169618cfb26193543290635c
Patrick Shushereba <pshushereba@gmail.com>
2021-05-05 21:28:04 -0700

    Started Authentication component.

---
+++ src/components/auth/Authentication.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div>
+      <form>
+        <div>
+          <div>
+            <label>First Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Last Name</label>
+            <input type="text" />
+          </div>
+          <div>
+            <label>Email</label>
+            <input type="email" />
+          </div>
+          <div>
+            <label>Password</label>
+            <input type="password" />
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mode: ["register", "login"],
+      activeMode: "",
+      form: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
